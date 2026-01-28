e15e3b2e9522705e7d5f1fcadad4bcdd090757fb
jiwonkimpark <jiwonkp@umich.edu>
2026-01-27 19:09:56 -0500

    remove idx from obj

--- scripts/preprocess_centaur.zsh
+++ scripts/preprocess_centaur.zsh
@@ -12,9 +12,11 @@ ZK_BIN=$DIR/target/$MODE/examples/zk
 modulus=28948022309329048855892746252171976963363056481941647379679742748393362948097
 
 function preprocess_r1cs {
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_own.zok r1cs --action spartan-setup --prover-key SCAN_OWN_P --verifier-key SCAN_OWN_V
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/well_formed.zok r1cs --action spartan-setup --prover-key WELL_FORMED_P --verifier-key WELL_FORMED_V
+#    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
+    $BIN --field-builtin pallas $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
+
+    $BIN --field-builtin pallas $DIR/examples/ZoKrates/centaur/scan_own.zok r1cs --action spartan-setup --prover-key SCAN_OWN_P --verifier-key SCAN_OWN_V
+    $BIN --field-builtin pallas $DIR/examples/ZoKrates/centaur/well_formed.zok r1cs --action spartan-setup --prover-key WELL_FORMED_P --verifier-key WELL_FORMED_V
 }
 
 preprocess_r1cs
