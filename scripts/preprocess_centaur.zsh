a58f460346538ea8e04c79052061842683720cf8
jiwonkimpark <jiwonkp@umich.edu>
2026-03-13 22:40:19 -0400

    Changed scan_new and scan_own (different states and signature check)

--- scripts/preprocess_centaur.zsh
+++ scripts/preprocess_centaur.zsh
@@ -8,15 +8,15 @@ MODE=release # debug or release
 DIR=$1
 BIN=$DIR/target/$MODE/examples/circ
 ZK_BIN=$DIR/target/$MODE/examples/zk
-CALLBACK_CAPACITY=$2
-BB_CAPACITY=$3
+#CALLBACK_CAPACITY=$2
+#BB_CAPACITY=$3
 
 modulus=28948022309329048855892746252171976963363056481941647379679742748393362948097
 
 function preprocess_r1cs {
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_own_$CALLBACK_CAPACITY.zok r1cs --action spartan-setup --prover-key SCAN_OWN_P --verifier-key SCAN_OWN_V
-    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/well_formed_$BB_CAPACITY.zok r1cs --action spartan-setup --prover-key WELL_FORMED_P --verifier-key WELL_FORMED_V
+    RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
+    RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/test.zok r1cs --action spartan-setup --prover-key SCAN_OWN_P --verifier-key SCAN_OWN_V
+#    $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/well_formed_$BB_CAPACITY.zok r1cs --action spartan-setup --prover-key WELL_FORMED_P --verifier-key WELL_FORMED_V
 }
 
 preprocess_r1cs
