8c791ab7561074c74b3c539aee5ac944bf7cb9e2
jiwonkimpark <jiwonkp@umich.edu>
2026-03-27 16:38:42 -0400

    add hash check of stateful witness values

--- scripts/preprocess_centaur.zsh
+++ scripts/preprocess_centaur.zsh
@@ -14,6 +14,7 @@ ZK_BIN=$DIR/target/$MODE/examples/zk
 modulus=28948022309329048855892746252171976963363056481941647379679742748393362948097
 
 function preprocess_r1cs {
+    RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/sig_test.zok r1cs --action spartan-setup --prover-key TEST_P --verifier-key TEST_V
     RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_new.zok r1cs --action spartan-setup --prover-key SCAN_NEW_P --verifier-key SCAN_NEW_V
     RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/scan_own.zok r1cs --action spartan-setup --prover-key SCAN_OWN_P --verifier-key SCAN_OWN_V
     RUST_BACKTRACE=full $BIN --field-custom-modulus $modulus $DIR/examples/ZoKrates/centaur/well_formed.zok r1cs --action spartan-setup --prover-key WELL_FORMED_P --verifier-key WELL_FORMED_V
